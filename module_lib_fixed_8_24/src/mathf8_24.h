9e92d3317ab9b41917d86048128165f377d70a30
Henk Muller <henk@xmos.com>
2011-03-24 16:31:47 +0000

    Added license stuff

--- module_lib_fixed_8_24/src/mathf8_24.h
+++ module_lib_fixed_8_24/src/mathf8_24.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2011, <insert copyright holder here>, All rights reserved
+// This software is freely distributable under a derivative of the
+// University of Illinois/NCSA Open Source License posted in
+// LICENSE.txt and at <http://github.xcore.com/>
+
 #define MATHF8_24_BITS 24
 #define MATHF8_24_IBITS (32-MATHF8_24_BITS)
 #define HALF    (1<<(MATHF8_24_BITS-1))
@@ -53,7 +58,28 @@ extern f8_24 divf8_24(f8_24 x, f8_24 y);
  **/
 extern f8_24 ldexpf8_24(f8_24 mant, int exp);
 
+/** frexpf8_24
+ *
+ * replacement of floating point frexp: computes a mantissa (0.5 <= m < 1)
+ * and an exponent of a fixed point number.
+ *
+ * \param d input value in 8_24
+ * \returns a mantissa as an f8_24 number
+ * \returns an exponent as an integer. d = m << exp
+ **/
 {f8_24,int} frexpf8_24 (f8_24 d);
+
+/** reducef8_24
+ *
+ * Returns the "middle" 8.24 bits form a 16.48 word. Useful for picking an
+ * answer out of a series of multiply accumulates. If an overflow has
+ * happened, the result is saturated to MINF8_24 or MAXF8_24
+ *
+ * \param h high input word twos complement 16.16 number
+ * \param l low input word to be concatenated with h to form 16.48 number
+ * \returns saturated (h:l)[bits 8..40]
+ **/
+f8_24 reducef8_24(int ynh, unsigned ynl);
 
 
 
@@ -75,39 +101,45 @@ extern f8_24 sinf8_24(f8_24 x);
  **/
 #define cosf8_24(x) sinf8_24(x+PIHALF)
 
+/** expf8_24
+ *
+ * Returns the exponent of a number
+ *
+ * \param x input value
+ * \returns e^x
+ **/
+extern f8_24 expf8_24(f8_24 x);
+
+/** logf8_24
+ *
+ * Returns the natural logarithm of a positive number.
+ *
+ * \param x input value
+ * \returns ln(x).
+ **/
+extern f8_24 logf8_24(f8_24 x);
+
+
+
+
 extern f8_24 sinehf8_24(f8_24 x, int cosine);
 
 #define sinhf8_24(x) sinehf8_24(x, 0)
 #define coshf8_24(x) sinehf8_24(x, 1)
 
-/** abssf8_24
- *
- * Returns the magnitude of a number
- *
- * \param x input value
- * \returns |x|
- **/
-extern f8_24 expf8_24(f8_24 x);
 extern f8_24 sqrtf8_24(f8_24 x);
-extern f8_24 logf8_24(f8_24 x);
 extern f8_24 log10f8_24(f8_24 x);
 
 
 
 
-/** reducef8_24
+
+/** printf8_24
  *
- * Returns the "middle" 8.24 bits form a 16.48 word. Useful for picking an
- * answer out of a series of multiply accumulates. If an overflow has
- * happened, the result is saturated to MINF8_24 or MAXF8_24
+ * prints a number using a decimal point, with an optional newline.
  *
- * \param h high input word twos complement 16.16 number
- * \param l low input word to be concatenated with h to form 16.48 number
- * \returns saturated (h:l)[bits 8..40]
+ * \param x input value
  **/
-f8_24 reducef8_24(int ynh, unsigned ynl);
-
-
 extern void printf8_24(f8_24 x);
 extern void printf8_24ln(f8_24 x);
 
