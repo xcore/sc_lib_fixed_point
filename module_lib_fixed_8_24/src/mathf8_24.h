3c4668897ca6d25329511dea009ef08af047c6b6
Henk Muller <henk@xmos.com>
2011-10-31 08:21:52 +0000

    Documentation of .h file

--- module_lib_fixed_8_24/src/mathf8_24.h
+++ module_lib_fixed_8_24/src/mathf8_24.h
@@ -3,116 +3,140 @@
 // University of Illinois/NCSA Open Source License posted in
 // LICENSE.txt and at <http://github.xcore.com/>
 
-#define MATHF8_24_BITS 24
+#define MATHF8_24_BITS (24)
 #define MATHF8_24_IBITS (32-MATHF8_24_BITS)
-#define HALF    (1<<(MATHF8_24_BITS-1))
-#define ONE     (HALF * 2)
-#define PI2     105414357
-#define PIHALF   26353589
 
-#define MINF8_24 0x80000000   // -128.000000000000000000000000
-#define MAXF8_24 0x7FFFFFFF   //  127.999999940395355224609375
 
-/*
- * Basic fixed point type is an integer.
+/** This type describes the fixed point numbers used in this library.
+ * Values are stored in 32 bits comprising a sign bit (bit 31), 7 mantissa
+ * bits (bits 30..24), and 24 fractional bits (bits 23..0). The value of a
+ * fixed point number is defined as the the signed integer value multiplied
+ * by 2^-24. There are no representations for *not-a-number* or *infinite*.
  */
 typedef int f8_24;
 
 
-/** fabsf8_24
- *
- * Returns the absolute value of a fixed point number
+/** This constant defines the smallest number that is defined in the fixed
+ * point range, which is -128.
+ */
+#define MINF8_24 (0x80000000)
+
+/** This constant defines the biggest number that is defined in the fixed
+ * point range, which is 127.999999940395355224609375
+ */
+#define MAXF8_24 (0x7FFFFFFF)
+
+/** This constant is the fixed point representation of the number 0.5
+ */
+#define HALF    (1<<(MATHF8_24_BITS-1))
+
+/** This constant is the fixed point representation of the number 1.0
+ */
+#define ONE     (HALF * 2)
+
+/** This constant is the closest fixed point representation of 2 PI.
+ */
+#define PI2     (105414357)
+
+/** This constant is the fixed point representation of PI/2
+ */
+#define PIHALF   (26353589)
+
+
+/** This function returns the absolute value of a fixed point number. The
+ * input value has to be in the range [-MAXF8_24..MAXF8_24]; the absolute
+ * value of MINF8_24 cannot be represented as a fixed point number.
  *
  * \param x input value
  * \returns |x|
  **/
 extern f8_24 fabsf8_24(f8_24 x);
 
-/** mulf8_24
+/** This function returns the product of two fixed point numbers. If the
+ * product does not fit in a fixed point number, a modulo answer is
+ * returned.
  *
- * Returns product of two fixed point numbers, both in two's complement 8.24 format
- *
- * \param x first input value in 8_24
- * \param y second input value in 8_24
+ * \param x First number to be multiplied
+ * \param y Second number to be multiplied
  * \returns x*y
  **/
 extern f8_24 mulf8_24(f8_24 x, f8_24 y);
 
-/** divf8_24
+/** This function returns the division of two fixed point numbers. If the
+ * product does not fit in a fixed point number, higher bits will be lost.
  *
- * Returns the division of two fixed point numbers, both in two's complement 8.24 format
- *
- * \param x first input value in 8_24
- * \param y second input value in 8_24
+ * \param x the divisor
+ * \param y the dividend
  * \returns x/y
  **/
 extern f8_24 divf8_24(f8_24 x, f8_24 y);
 
-/** ldexpf8_24
+/** This function is the fixed point equivalent of ldexp(): given a
+ * mantissa and an exponent it computes the fixed point number with the
+ * value mant * 2 ^ exp. The reverse operation is implemented by frexp8_24().
  *
- * replacement of floating point ldexp: computes mant * 2 ^ exp
- *
- * \param mant mantissa input value in 8_24
- * \param exp exponent input value, signed integer
+ * \param mant mantissa input value represented as a fixed point number
+ * \param exp exponent input value, represented as a signed integer
  * \returns math * 2 ^ exp
+ * \sa frexpf8_24
  **/
 extern f8_24 ldexpf8_24(f8_24 mant, int exp);
 
-/** frexpf8_24
+/** This functions splits a fixed point number into a mantissa and
+ * exponent. The mantissa is either zero or in the range [0.5..1). The
+ * exponent is in the range [-32..32]. and an exponent of a fixed point
+ * number. The reverse operation is implemented by ldexp8_24().
  *
- * replacement of floating point frexp: computes a mantissa (0.5 <= m < 1)
- * and an exponent of a fixed point number.
- *
- * \param d input value in 8_24
- * \returns a mantissa as an f8_24 number
- * \returns an exponent as an integer. d = m << exp
+ * \param d input fixed point number
+ * \returns a mantissa as an fixed point number
+ * \returns an exponent as an integer.
+ * \sa ldexpf8_24
  **/
 {f8_24,int} frexpf8_24 (f8_24 d);
 
-/** reducef8_24
+/** This function clips and rounds a 16.48 fixed point number into an 8.24
+ * fixed point number. It is used for finalising the answer out of a series
+ * of multiply accumulates. If an overflow has happened, the result is
+ * saturated to MINF8_24 or MAXF8_24. The number is rounded towards minus
+ * infinity; if rounding is required half should be added to the number,
+ * typically before the multiply accumulate
  *
- * Returns the "middle" 8.24 bits form a 16.48 word. Useful for picking an
- * answer out of a series of multiply accumulates. If an overflow has
- * happened, the result is saturated to MINF8_24 or MAXF8_24
- *
- * \param h high input word twos complement 16.16 number
+ * \param h high input word (two's complement 16.16 number)
  * \param l low input word to be concatenated with h to form 16.48 number
  * \returns saturated (h:l)[bits 8..40]
  **/
-f8_24 reducef8_24(int ynh, unsigned ynl);
+f8_24 reducef8_24(int h, unsigned l);
 
 
 
-/** sinf8_24
- *
- * Returns the sine of a fixed poiont number in radians.
+/** This function returns the sine of a fixed point number in radians. The
+ * input number has to be in the range -MAXF8_24 + PI and MAXF8_24 - PI.
  *
  * \param x input value in radians
  * \returns sine(x)
  **/
 extern f8_24 sinf8_24(f8_24 x);
 
-/** cosf8_24
- *
- * Returns the cosine of a fixed poiont number in radians.
+/** This function returns the cosine of a fixed point number in radians. The
+ * input number has to be in the range -MAXF8_24 + PI and MAXF8_24 - PI.
  *
  * \param x input value in radians
  * \returns cosine(x)
  **/
-#define cosf8_24(x) sinf8_24(x+PIHALF)
+#define cosf8_24(x) sinf8_24((x)+PIHALF)
 
-/** expf8_24
- *
- * Returns the exponent of a number
+/** This function returns the natural exponent of a fixed point number. The
+ * input number has to be less than 4.8, otherwise the answer cannot be
+ * represented as a fixed point number. For input values larger than 3
+ * there is a relatively large error in the last three bits of the answer.
  *
  * \param x input value
  * \returns e^x
  **/
 extern f8_24 expf8_24(f8_24 x);
 
-/** logf8_24
- *
- * Returns the natural logarithm of a positive number.
+/** This function returns the natural logarithm (ln) of a fixed point number. The
+ * input number has to be positive.
  *
  * \param x input value
  * \returns ln(x).
@@ -124,27 +148,81 @@ extern f8_24 logf8_24(f8_24 x);
 
 extern f8_24 sinehf8_24(f8_24 x, int cosine);
 
-#define sinhf8_24(x) sinehf8_24(x, 0)
-#define coshf8_24(x) sinehf8_24(x, 1)
+/** This function returns the hyperbolic sine (sinh) of a fixed point
+ * number. The input number has to be in the range [-5.5..5.5] in order to
+ * avoid overflow, and for values outside the [-4..4] range there are
+ * relatively large errors in the result.
+ *
+ * \param x input value
+ * \returns sinh(x)
+ **/
+#define sinhf8_24(x) sinehf8_24((x), 0)
 
+/** This function returns the hyperbolic cosine (cosh) of a fixed point
+ * number. The input number has to be in the range [-5.5..5.5] in order to
+ * avoid overflow, and for values outside the [-4..4] range there are
+ * relatively large errors in the result.
+ *
+ * \param x input value
+ * \returns sinh(x)
+ **/
+#define coshf8_24(x) sinehf8_24((x), 1)
+
+/** This function returns the square root of a fixed point number.
+ * The input number has to be positive.
+ *
+ * \param x input value in radians
+ * \returns sqrt(x)
+ **/
 extern f8_24 sqrtf8_24(f8_24 x);
+
+/** This function returns the logarithm base 10 of a fixed point number. The
+ * input number has to be positive.
+ *
+ * \param x input value
+ * \returns ln(x)/ln(10).
+ **/
 extern f8_24 log10f8_24(f8_24 x);
 
 
 
 
 
-/** printf8_24
+/** This function prints a number using a decimal point.
  *
- * prints a number using a decimal point, with an optional newline.
- *
- * \param x input value
+ * \param x value to be printed
  **/
 extern void printf8_24(f8_24 x);
+
+/** This function prints a number using a decimal point and adds a newline.
+ *
+ * \param x value to be printed
+ **/
 extern void printf8_24ln(f8_24 x);
 
-#define f8_242int(a) (a >> MATHF8_24_BITS)
 
-#define int2f8_24(a) (a << MATHF8_24_BITS)
+/** This function converts a fixed point number to an integer, discarding
+ * the fractional part.
+ *
+ * \param x fixed point input value
+ * \returns integer equivalent
+ **/
+#define f8_242int(x) ((x) >> MATHF8_24_BITS)
 
+/** This function converts an integer to a fixed point number. The input
+ * number has to be in the range [-128..127]
+ *
+ * \param x integer input value
+ * \returns fixed point equivalent
+ **/
+#define int2f8_24(x) ((x) << MATHF8_24_BITS)
+
+
+/** This function rounds a fixed point number to its nearest integral
+ * number and returns a fixed point number. The input number should be less
+ * than 127.5.
+ *
+ * \param x input value
+ * \returns nearest integer.
+ **/
 f8_24 froundf8_24(f8_24 x);
